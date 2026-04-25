481e55f95efc2cbbe2e3e5c7ee16dc21dc6bdbf5
vjnicius01 <vinimedeirosdeoliveira01@gmail.com>
2026-04-24 22:37:55 -0300

    Update cadastro.js

--- api /cadastro.js
+++ api /cadastro.js
@@ -1,12 +1,10 @@
-import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ erro: 'Método não permitido' });
   }
-
   const { nome, login, senha } = req.body;
-
   try {
     const conexao = await mysql.createConnection({
       host: 'y4cyfw.h.filess.io',
@@ -15,19 +13,16 @@ export default async function handler(req, res) {
       password: 'd5844738c1ec7e694177c266cc66f8d07b6aaf05',
       database: 'sistema_licencas_norhungry'
     });
-
     await conexao.execute(
       'INSERT INTO tbUsuarios (nome, login, senha) VALUES (?, ?, ?)',
       [nome, login, senha]
     );
-
     await conexao.end();
     return res.status(200).json({ sucesso: true });
-
   } catch (err) {
     if (err.code === 'ER_DUP_ENTRY') {
       return res.status(400).json({ sucesso: false, erro: 'Login já cadastrado!' });
     }
-    return res.status(500).json({ sucesso: false, erro: 'Erro interno' });
+    return res.status(500).json({ sucesso: false, erro: err.message });
   }
-}
+};
