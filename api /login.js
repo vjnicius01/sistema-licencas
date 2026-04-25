a544fd976c969562d124c8817fb1a5ee91ffef51
vjnicius01 <vinimedeirosdeoliveira01@gmail.com>
2026-04-24 22:38:53 -0300

    Update login.js

--- api /login.js
+++ api /login.js
@@ -1,13 +1,10 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ erro: 'Método não permitido' });
   }
-
   const { login, senha } = req.body;
-
   if (login === 'admin' && senha === '123456') {
     return res.status(200).json({ sucesso: true });
   }
-
   return res.status(401).json({ sucesso: false });
 };
